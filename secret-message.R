5d8f3e5df3209412c2968784cb636222e30762f5
Georges Labrèche <georges@opendatakosovo.org>
2019-07-03 16:42:30 +0200

    Update secret-message.R

--- secret-message.R
+++ secret-message.R
@@ -1,6 +1,6 @@
 # Define a function that will display the secret message.
 plot_secret_message = function(){
-
+  
   # Display the secret message in a new window.
   dev.new()
   
@@ -11,17 +11,33 @@ plot_secret_message = function(){
        axes=FALSE,
        xlab='',
        ylab='')
-
-  # The range of values.
-  t = seq(0, 360, len=360)
   
-  # The line functions.
-  x = 16 * sin(t*pi/180)^3
-  y = 13 * cos(t*pi/180) - 5*cos(2*t*pi/180) - 2*cos(3*t*pi/180) - cos(4*t*pi/180)
+  # Define the function that will calculate the plot's X values.
+  x_values = function(t){
+    16 * sin(t)^3
+  }
   
-  # Plot the line functions.
-  lines(x, y, col='red', lwd=4)
-  lines(-x, y, col='red', lwd=4)
+  # Define the function that will calculate the plot's Y values.
+  y_values = function(t){
+    13 * cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t)
+  }
+  
+  # Build the ranges of t values that will serve as 
+  # input to the x_values and y_values functions.
+  # Convert them to radians by multiplying with pi/180.
+  t = seq(0, 360, len=360) * pi/180
+  
+  # Calculate our x and y values using the functions we defined.
+  x = x_values(t)
+  y = y_values(t)
+  
+  # Plot the results as a line plot.
+  lines(x, y, col='red', lty=1, lwd=3)
+  
+  # Things to try:
+  #   1. What happens when you change the value of lty and lwd?
+  #   2. Play with the x_values and y_values functions.
+  #       What happens when you change some numbers or remove parts of it?
 }
 
 plot_secret_message()
