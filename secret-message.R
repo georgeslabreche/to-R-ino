26263174fe0152659c67c592ce02e756117368f8
Georges Labrèche <georges@opendatakosovo.org>
2019-07-03 17:01:44 +0200

    Update secret-message.R

--- secret-message.R
+++ secret-message.R
@@ -1,44 +1,57 @@
-# Define a function that will display the secret message.
-plot_secret_message = function(){
-  
-  # Display the secret message in a new window.
-  dev.new()
-  
-  # Define our plotting box by drawing an empty plot.
-  plot(x=0, y=0,
-      xlim=c(-20, 20),
-      ylim=c(-20, 20),
-      type='n',
-      axes=FALSE,
-      xlab='',
-      ylab='')
-  
-  # Define the function that will calculate the plot's X values.
-  x_values = function(t){
-    16 * sin(t)^3
-  }
-  
-  # Define the function that will calculate the plot's Y values.
-  y_values = function(t){
-    13 * cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t)
-  }
-  
-  # Build the ranges of t values that will serve as 
-  # input to the x_values and y_values functions.
-  # Convert them to radians by multiplying with pi/180.
-  t = seq(0, 360, len=360) * pi/180
-  
-  # Calculate our x and y values using the functions we defined.
-  x = x_values(t)
-  y = y_values(t)
-  
-  # Plot the results as a line plot.
-  lines(x, y, col='red', lty=1, lwd=3)
-  
-  # Things to try:
-  #   1. What happens when you change the value of lty and lwd?
-  #   2. Play with the x_values and y_values functions.
-  #       What happens when you change some numbers or remove parts of it?
+################################################
+# 1. Define how to get X and Y values to plot. #
+################################################
+
+# Define the function that will calculate the plot's X values.
+calculate_x = function(t){
+  16 * sin(t)^3
 }
 
-plot_secret_message()
+# Define the function that will calculate the plot's Y values.
+calculate_y = function(t){
+  13 * cos(t) - 5*cos(2*t) - 2*cos(3*t) - cos(4*t)
+}
+
+
+################################
+# 2. Calculate X and Y values. #
+################################
+
+# Build the ranges of t values that will serve as 
+# input to the calculate_x and calculate_y functions.
+# Convert them to radians by multiplying with pi/180.
+input_sample = seq(0, 360, len=360) * pi/180
+
+# Calculate our x and y values using the functions we defined.
+x = calculate_x(input_sample)
+y = calculate_y(input_sample)
+
+
+#######################################
+# 3. Plot the calcualted data points. #
+#######################################
+
+# Display the secret message in a new window.
+dev.new()
+
+# Define our plotting box by drawing an empty plot.
+plot(x=0, y=0,
+    xlim=c(-20, 20),
+    ylim=c(-20, 20),
+    type='n',
+    axes=FALSE,
+    xlab='',
+    ylab='')
+
+# Plot the results as a line plot.
+lines(x, y, col='red', lty=1, lwd=3)
+
+
+##################
+# 4. Experiment. #
+##################
+
+# Things to try:
+#   1. What happens when you change the value of lty and lwd?
+#   2. Play with the calculate_x and calculate_y functions.
+#       What happens when you change some numbers or remove parts of it?
